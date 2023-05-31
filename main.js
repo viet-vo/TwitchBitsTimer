60ad4b6a4d8329e266147a7f7592140d9bf12cc0
Viet Vo <viet.allen.qvo@gmail.com>
2023-05-30 23:40:55 -1000

    Started timer window with a bootstrapped Electron app.

---
+++ main.js
@@ -0,0 +1,33 @@
+const { app, BrowserWindow, Menu } = require("electron");
+const path = require("path");
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+    },
+    autoHideMenuBar: true,
+    titleBarStyle: "hidden",
+    titleBarOverlay: true,
+  });
+
+  win.loadFile("index.html");
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
