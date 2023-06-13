85a0af31aee8f0a1568f65e6bbbf5eb201a00494
Viet Vo <viet.allen.qvo@gmail.com>
2023-06-12 22:50:36 -1000

    added a proper indirect site to a github page. Next need to configure authentication link with proper configs and perms

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const { app, BrowserWindow, Menu } = require("electron");
 //const { URL, URLSearchParams } = require('url');
 const path = require("path");
 const fetch = require("electron-fetch").default;
+
 const gotTheLock = app.requestSingleInstanceLock();
 
 if (!gotTheLock) {
@@ -28,10 +29,23 @@ app.on("second-instance", (event, commandLine, workingDirectory) => {
   }
 });
 
+/*
+Custom URI Handler
+*/
+if (process.defaultApp) {
+  if (process.argv.length >= 2) {
+    app.setAsDefaultProtocolClient("aiyo_timer", process.execPath, [
+      path.resolve(process.argv[1]),
+    ]);
+  }
+} else {
+  app.setAsDefaultProtocolClient("aiyo_timer");
+}
+
 function createWindow() {
   const win = new BrowserWindow({
     width: 400,
-    height: 125,
+    height: 200,
     webPreferences: {
       preload: path.join(__dirname, "preload.js"),
     },
@@ -62,3 +76,34 @@ app.on("window-all-closed", () => {
     app.quit();
   }
 });
+
+function runToken(token) {
+  let client_id = "";
+  fetch("https://id.twitch.tv/oauth2/validate", {
+    headers: {
+      Accept: "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  })
+    .then((resp) => resp.json())
+    .then((resp) => {
+      client_id = resp.client_id;
+      return fetch("https://api.twitch.tv/helix/users", {
+        headers: {
+          Accept: "application/json",
+          "Client-ID": client_id,
+          Authorization: `Bearer ${token}`,
+        },
+      });
+    })
+    .then((resp) => resp.json())
+    .then((resp) => {
+      console.log("Got user", resp);
+
+      // ipc
+      win.webContents.send("twitch_user", resp.data[0]);
+    })
+    .catch((err) => {
+      console.log("An Error Occurred", err);
+    });
+}
---
+++ script.js
@@ -0,0 +1,25 @@
+console.log('loaded');
+
+document.addEventListener('click', (e) => {
+    if (e.target.tagName == 'A') {
+        e.preventDefault();
+        window.electron.openWeb(e.target.getAttribute('href'));
+    }
+});
+
+// Display response object in table form
+// window.electron.onTwitchUser(user => {
+// 	let table = document.getElementById('user');
+// 	table.textContent = '';
+
+//     for (var key in user) {
+//         var tr = document.createElement('tr');
+//         table.append(tr);
+//         var td = document.createElement('td');
+//         td.textContent = key;
+//         tr.append(td);
+//         var td = document.createElement('td');
+//         td.textContent = user[key];
+//         tr.append(td);
+//     }
+// });
